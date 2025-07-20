51df41a16d92853ac15b2be82fbf6dd1d9833b93
Shawn Kemp <shawn.kemp@gmail.com>
2025-07-19 19:28:55 -0700

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ if(new URLSearchParams(window.location.search).get('s')){qsize = new URLSearchPa
 var qcomplexity = R.random_int(1,10);
 if(new URLSearchParams(window.location.search).get('d')){qcomplexity = new URLSearchParams(window.location.search).get('d')}; //size
 
-var qorientation =R.random_int(1,2) < 1 ? "portrait" : "landscape";
+var qorientation =R.random_int(1,2) < 2 ? "portrait" : "landscape";
 var qframecolor = R.random_int(1,3) < 1 ? "White" : R.random_int(1,3) < 2 ? "Mocha" : "Random";
 var qmeshDensity = R.random_int(5,15);
 var qjiggle = R.random_int(25,100);
